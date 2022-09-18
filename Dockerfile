0446aa35be9f3d3d9bc2dd2bdf200c4e72b67b77
Ben <toussaint.benoit@gmail.com>
2022-09-18 14:21:07 +0200

    added env, k8s secret and configmap

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 # syntax=docker/dockerfile:1
 FROM python:3.9.13-slim-bullseye
 RUN python -m pip install --upgrade pip
+WORKDIR /app
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-
-WORKDIR /app
 
 COPY /src/ .
 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 import pandas as pd
-
+import configparser
+import os
 
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('config/config.ini')
     test_df = pd.DataFrame()
-    print('Hello there')
+    username = os.getenv('DP_USER')
+    print(username)
+    print(f'Hello there first param: {config["SECTION"]["first"]}')
